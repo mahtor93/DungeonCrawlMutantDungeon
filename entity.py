4d5b94eae20713a2f10d17cd9019fa34ae05b1ad
Mario H. Tornería R. <mh.torneria@gmail.com>
2023-11-16 18:50:23 -0300

    base de una dungeon con generación de mapas aleatorios

---
+++ entity.py
@@ -0,0 +1,16 @@
+from typing import Tuple
+
+class Entity:
+    """
+    Objeto generico que representa jugadores, enemigos, items, etc.
+    """
+    
+    def __init__(self, x:int, y:int,char:str,color:Tuple[int,int,int]):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+        
+    def move(self, dx:int, dy:int) -> None:
+        self.x += dx
+        self.y += dy
